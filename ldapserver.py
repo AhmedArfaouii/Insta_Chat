a715b9de47db880ded05a7594e7a906f3f37a70d
AhmedArfaouii <ahmed.arfaouii11@gmail.com>
2024-01-04 14:58:14 +0100

    Decryption_not_working

--- ldapserver.py
+++ ldapserver.py
@@ -1,34 +1,16 @@
-import pika
+import tkinter as tk
 import ldap
 import hashlib
 
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return hashed_password
+
 class LDAPServer:
-    
     ou_dn = 'ou=users,dc=python666,dc=local'
+
     def __init__(self):
         self.ldap_connection = None
-
-    def get_user_info(self):
-        print("Please enter user information:")
-        login = input("Login: ")
-        first_name = input("First Name: ")
-        last_name = input("Last Name: ")
-        email = input("Email: ")
-        password = input("Password: ")  # Consider using getpass.getpass() for password input
-
-        user_data = {
-            'login': login,
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'password': password
-        }
-
-        return user_data
-
-    def hash_password(self, password):
-        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        return hashed_password
 
     def ldap_initialize(self):
         ldap_server = 'ldap://192.168.162.135'
@@ -37,16 +19,18 @@ class LDAPServer:
         ldap_admin_password = 'looklook11'
 
         try:
-            ldap_conn = ldap.initialize(ldap_server)
-            ldap_conn.simple_bind_s(ldap_admin_dn, ldap_admin_password)
+            self.ldap_connection = ldap.initialize(ldap_server)
+            self.ldap_connection.simple_bind_s(ldap_admin_dn, ldap_admin_password)
             print("LDAP Connection initialized!")
-            return ldap_conn, ldap_base_dn
+            return self.ldap_connection, ldap_base_dn
         except ldap.LDAPError as e:
             print(f"Error initializing LDAP connection: {e}")
             return None, None
 
-    def add_user_to_ldap(self, ldap_conn, ldap_base_dn):
-        user_data = self.get_user_info()
+    def add_user_to_ldap(self, user_data):
+        if not self.ldap_connection:
+            self.ldap_initialize()
+
         try:
             attrs = [
                 ('objectClass', [b'top', b'person', b'organizationalPerson', b'inetOrgPerson']),
@@ -60,67 +44,18 @@ class LDAPServer:
 
             dn = f'cn={user_data["login"]},{self.ou_dn}'
 
-            ldap_conn.add_s(dn, attrs)
+            self.ldap_connection.add_s(dn, attrs)
             print(f"User {user_data['login']} successfully added to the LDAP server.")
         except ldap.LDAPError as e:
             print(f"Error adding user to the LDAP server: {e}")
-
-    def authenticate_user(self, ldap_conn):
-        login = input("Enter your login: ")
-        initial_password = input("Enter your password: ")
-
-        try:
-            base_dn = 'dc=python666,dc=local'
-            search_filter = f'(uid={login})'
-            result = ldap_conn.search_s(base_dn, ldap.SCOPE_SUBTREE, search_filter)
-
-            if not result:
-                print("User not found.")
-                return False
-
-            stored_password = result[0][1]['userPassword'][0].decode('utf-8')
-            hashed_password = self.hash_password(initial_password)
-
-            if stored_password == hashed_password:
-                print("Authentication successful.")
-                return True
-            else:
-                print("Authentication failed.")
-                return False
-        except ldap.LDAPError as e:
-            print(f"Error authenticating user: {e}")
-            return False
-
-    def check_rabbitmq_connection(self):
-        try:
-            login = input("Enter your login: ")
-            initial_password = input("Enter your password: ")
-            password = self.hash_password(initial_password)
-            credentials = pika.PlainCredentials(login, password)
-            parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
-
-            connection = pika.BlockingConnection(parameters)
-            connection.close()
-            print("Connection to RabbitMQ established successfully: All good!")
-        except Exception as e:
-            print(f"Failed to connect to RabbitMQ: {e}")
 
     def close_connection(self):
         if self.ldap_connection:
             self.ldap_connection.unbind_s()
             print("LDAP Connection closed")
 
+    def hash_password(self, password):
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        return hashed_password
 
 
-# # Create an instance of the LDAPServer class
-# ldap_server = LDAPServer()
-
-# # Initialize LDAP connection
-# ldap_connection, ldap_base_dn = ldap_server.ldap_initialize()
-
-# # Add user to LDAP
-# if ldap_connection:
-#     ldap_server.add_user_to_ldap(ldap_connection, ldap_base_dn)
-
-# # Close LDAP connection
-# ldap_server.close_connection()
---
+++ tkinter_add_user.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from ldapserver import LDAPServer
+
+def add_user(entries, ldap_server):
+    user_data = {}
+    for entry in entries:
+        field = entry[0]
+        value = entry[1].get()
+        user_data[field] = value
+
+    # Ensure the correct keys are used to create the user_data dictionary
+    keys = ['login', 'first name', 'last name', 'email', 'password']
+    for key in keys:
+        if key not in user_data:
+            print(f"Missing {key.capitalize()} field.")
+            return
+
+    # Rename keys to match LDAP attribute names
+    user_data['first_name'] = user_data.pop('first name')
+    user_data['last_name'] = user_data.pop('last name')
+
+    ldap_server.add_user_to_ldap(user_data)
+
+
+def create_user_gui():
+    root = tk.Tk()
+    root.title("Add User to LDAP")
+
+    # Define style for labels and buttons
+    label_style = ('Helvetica', 12)
+    button_style = ('Helvetica', 12, 'bold')
+
+    # Configure root window background color
+    root.configure(bg='#F0F0F0')
+
+    ldap_server = LDAPServer()
+    ldap_server.ldap_initialize()
+
+    # Labels and entry fields for user information
+    labels = ['Login:', 'First Name:', 'Last Name:', 'Email:', 'Password:']
+    entries = []
+
+    for i, label_text in enumerate(labels):
+        label = tk.Label(root, text=label_text, font=label_style, bg='#F0F0F0')
+        label.grid(row=i, column=0, padx=10, pady=5, sticky='w')
+        entry = tk.Entry(root, font=label_style)
+        entry.grid(row=i, column=1, padx=10, pady=5, sticky='e')
+        entries.append((label_text[:-1].lower(), entry))
+
+    # Button to add user
+    add_button = tk.Button(root, text="Add User", command=lambda: add_user(entries, ldap_server),
+                           font=button_style, bg='#4CAF50', fg='white')
+    add_button.grid(row=len(labels), columnspan=2, pady=10)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    create_user_gui()
